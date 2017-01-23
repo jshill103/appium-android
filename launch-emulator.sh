24d4b0b9229d6982e3cf02cd6dbc562fb91f0453
Jared Shillingburg <i20793@jshillingburg-1.xactware.com>
2017-01-23 14:28:45 -0700

    fix to kill script after done running

--- launch-emulator.sh
+++ launch-emulator.sh
@@ -4,7 +4,7 @@
 /usr/sbin/sshd
 adb start-server
 
-echo "no" | /usr/local/android-sdk-linux/tools/emulator64-x86 -avd pix -sdcard qasdcard.img -noaudio -no-window -gpu off -verbose -qemu -usbdevice tablet
+echo "no" | /usr/local/android-sdk-linux/tools/emulator64-x86 -avd pix -sdcard qasdcard.img -noaudio -no-window -gpu off -verbose -qemu -usbdevice tablet &
 echo "Started emulator"
 
 sleep 90
