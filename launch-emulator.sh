5a6abbbb6ebceb778db1f41ebab4115e57588575
Jared Shillingburg <i20793@jshillingburg-1.xactware.com>
2017-01-31 14:35:04 -0700

    help

--- launch-emulator.sh
+++ launch-emulator.sh
@@ -7,12 +7,12 @@ adb start-server
 echo "no" | /usr/local/android-sdk-linux/tools/emulator64-x86 -avd pix -sdcard qasdcard.img -noaudio -no-window -gpu off -verbose -qemu -usbdevice tablet &
 echo "Started emulator"
 
-sleep 120
+sleep 180
+
 #SENTINEL=0
-#DEVICE="offline"
-#while [$SENTINEL -lt 1 ]; do
+#while [ $SENTINEL -lt 1 ]; do
 #	DEVICE=$(adb devices | grep 'device' | cut -d'4' -f2 |tail -n +2 | awk '{print $1}')
-#	if [ $DEVICE = "device"]; then
+#	if [ $DEVICE = "device" ]; then
 #		let SENTINEL=SENTINEL+1
 #	fi
 #done
