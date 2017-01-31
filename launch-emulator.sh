60d76ca9cf9411229f7083571e03ff17f3c8f68b
Jared Shillingburg <i20793@jshillingburg-1.xactware.com>
2017-01-31 09:52:08 -0700

    test

--- launch-emulator.sh
+++ launch-emulator.sh
@@ -4,9 +4,9 @@
 /usr/sbin/sshd
 adb start-server
 
-echo "no" | /usr/local/android-sdk-linux/tools/emulator64-x86 -avd pix -sdcard qasdcard.img -noaudio -no-window -gpu off -verbose -qemu -usbdevice tablet &
+echo "no" | /usr/local/android-sdk-linux/tools/emulator64-x86 -avd pix -sdcard qasdcard.img -noaudio -no-window -gpu off -verbose -qemu -usbdevice tablet
 echo "Started emulator"
 
-sleep 120
+sleep 90
 
 mono /runner/automation.xactimate.Android/Android/Tests/bin/USRelease/Tests.exe $ONE $TWO $THREE
