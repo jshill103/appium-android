d88e02228b5c5579f38a481c80f27a9bcb028a30
Jared Shillingburg <i20793@jshillingburg-1.xactware.com>
2017-01-31 13:06:50 -0700

    uhhhg

--- launch-emulator.sh
+++ launch-emulator.sh
@@ -4,9 +4,19 @@
 /usr/sbin/sshd
 adb start-server
 
-echo "no" | /usr/local/android-sdk-linux/tools/emulator64-x86 -avd pix -sdcard qasdcard.img -noaudio -no-window -gpu off -verbose -qemu -usbdevice tablet
+echo "no" | /usr/local/android-sdk-linux/tools/emulator64-x86 -avd pix -sdcard qasdcard.img -noaudio -no-window -gpu off -verbose -qemu -usbdevice tablet &
 echo "Started emulator"
 
-sleep 90
+sleep 120
+#SENTINEL=0
+#DEVICE="offline"
+#while [$SENTINEL -lt 1 ]; do
+#	DEVICE=$(adb devices | grep 'device' | cut -d'4' -f2 |tail -n +2 | awk '{print $1}')
+#	if [ $DEVICE = "device"]; then
+#		let SENTINEL=SENTINEL+1
+#	fi
+#done
+
 
 mono /runner/automation.xactimate.Android/Android/Tests/bin/USRelease/Tests.exe $ONE $TWO $THREE
+sleep 120
